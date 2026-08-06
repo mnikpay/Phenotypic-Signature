def848ffb7b9c53ff1f23cb9d4543fc46c6a272e
mnikpay <60237353+mnikpay@users.noreply.github.com>
2026-08-06 15:07:03 -0400

    Add files via upload

---
+++ calculate_cor.r
@@ -0,0 +1,45 @@
+args <- commandArgs(TRUE)
+
+a1=paste(args[1],sep="")
+
+
+a2=paste(a1,".results.txt",sep="")
+
+dat=read.table("psign_v1.dat", header=T, row.names=1)
+
+geneX <- a1
+
+x <- as.numeric(dat[geneX, ])
+
+
+results <- lapply(rownames(dat), function(g){
+
+y <- as.numeric(dat[g, ])
+
+n <- sum(complete.cases(x, y))
+
+
+tmp <- cor.test(x,y, method="spearman")
+
+rho <- unname(tmp$estimate)
+
+se <- sqrt((1 - rho^2) / (n - 2))
+
+    data.frame(
+        Gene = g,
+        N= n,
+        rho = tmp$estimate,
+                SE=se,
+                abs_rho = abs(tmp$estimate),
+        P = tmp$p.value
+
+    )
+
+
+})
+
+results <- do.call(rbind, results)
+
+
+write.table(results, file = a2, quote = FALSE, sep=" ", row.names=F)
+
